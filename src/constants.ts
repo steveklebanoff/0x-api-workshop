a840a83d3c28c6d6b85106d588941c9fe45a2801
Steve Klebanoff <steve@0xproject.com>
2020-03-01 12:37:04 -0800

    abstracting

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,11 @@
-import { Web3Wrapper } from "@0x/web3-wrapper";
-import { BigNumber } from "@0x/utils";
-
-import { getTokenAddress } from "./tokens";
 
 export const NETWORK_ID = 42;
+
 export const PUBLIC_KEY = process.env.PUBLIC_KEY!;
+if (!PUBLIC_KEY) { throw new Error("Please specify a public key"); }
+
 export const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+if (!PRIVATE_KEY) { throw new Error("Please specify a private key"); }
+
 export const RPC_URL = process.env.RPC_URL!;
-
-export const BUY_TOKEN_ADDRESS = getTokenAddress("MKR");
-export const SELL_TOKEN_ADDRESS = getTokenAddress("DAI");
-export const SELL_TOKEN_AMOUNT_WEI = Web3Wrapper.toWei(
-  new BigNumber(0.1)
-).toNumber(); // NOTE: this assumes 18 decimals for the given token
-
-if (!PRIVATE_KEY) {
-  throw new Error("Please specify a private key");
-}
-
-if (!PUBLIC_KEY) {
-  throw new Error("Please specify a public key");
-}
-
-if (!RPC_URL) {
-  throw new Error("Please specify a RPC URL");
-}
+if (!RPC_URL) { throw new Error("Please specify a RPC URL"); }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { Web3Wrapper } from "@0x/web3-wrapper";
-import { checkAndSetAllowance } from "./allowances";
+import { BigNumber } from "@0x/utils";
 import { ContractWrappers } from "@0x/contract-wrappers";
+
+import { checkAndSetAllowance } from "./allowances";
 import {
   NETWORK_ID,
-  SELL_TOKEN_ADDRESS,
   PUBLIC_KEY,
-  BUY_TOKEN_ADDRESS,
-  SELL_TOKEN_AMOUNT_WEI
 } from "./constants";
 import { getProviderEngine } from "./provider";
 import { trade } from "./trade";
+import { getTokenAddress } from "./tokens";
 
 const start = async () => {
+  const buyTokenAddress = getTokenAddress("MKR");
+  const sellTokenAddress = getTokenAddress("DAI");
+  const sellTokenUnitAmount = 0.25;
+
   // Set up wallet and provider
   const providerEngine = getProviderEngine();
   const contractWrappers = new ContractWrappers(providerEngine, {
@@ -20,13 +24,14 @@ const start = async () => {
   const web3Wrapper = new Web3Wrapper(providerEngine);
 
   // Ensure we have allowances set
-  await checkAndSetAllowance(SELL_TOKEN_ADDRESS, contractWrappers, providerEngine);
+  await checkAndSetAllowance(sellTokenAddress, contractWrappers, providerEngine);
 
   // Send out trade
+  const sellTokenAmountWei = Web3Wrapper.toWei(new BigNumber(sellTokenUnitAmount)).toNumber();
   await trade(
     web3Wrapper, PUBLIC_KEY,
-    { buyTokenAddress: BUY_TOKEN_ADDRESS, sellTokenAddress: SELL_TOKEN_ADDRESS },
-    SELL_TOKEN_AMOUNT_WEI
+    { buyTokenAddress: buyTokenAddress, sellTokenAddress: sellTokenAddress },
+    sellTokenAmountWei
   );
 };
 
